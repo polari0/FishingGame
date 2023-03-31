18c41b4b7874829fefd829b521f446212fa1964f
Ilmari Vainio <ilmari.vainio01@gmail.com>
2023-03-31 15:16:41 +0300

    And final few changes for now

--- Assets/Scripts/UIScripts/InventoryUpdate.cs
+++ Assets/Scripts/UIScripts/InventoryUpdate.cs
@@ -26,8 +26,33 @@ public class InventoryUpdate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //UpdateDiscplay();
+        UpdateDisplay();
     }
+
+
+    //Updates the display when game is running 
+    public void UpdateDisplay()
+    {
+        for (int i = 0; i < inventory.Container.Count; i++)
+        {
+            if (itemsDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                itemsDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("no"); 
+            }
+            else
+            {
+                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+                itemsDisplayed.Add(inventory.Container[i], obj);
+
+            }
+        }
+
+    }
+
+
+    //Creates a display for the inventory when it is loaded
 
     public void CreateDisplay()
     {
@@ -36,12 +61,16 @@ public class InventoryUpdate : MonoBehaviour
             var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
             obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
             obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            itemsDisplayed.Add(inventory.Container[i], obj);
         }
 
        
     }
+
+    //sets positions for the items with in the inventory 
     public Vector3 GetPosition(int i)
     {
         return new Vector3(startX + (spaceBetweenItemX * (i % numberOfColums)),StartY + ((-spaceBetweenItemY * (i / numberOfColums))), 0f);
     }
+
 }
